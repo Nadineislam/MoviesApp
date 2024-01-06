83712d52d977492eb34fd3aecaebb34a692a61d0
Nadine Islam <nadeen.kamaal@gmail.com>
2024-01-06 18:31:21 +0200

    Add star icon next to movie vote text in MovieDetails

--- app/src/main/java/com/example/movieapp/movie_home_feature/presentation/activities/MovieDetails.kt
+++ app/src/main/java/com/example/movieapp/movie_home_feature/presentation/activities/MovieDetails.kt
@@ -11,12 +11,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -77,8 +82,17 @@ fun MovieDetails(
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Text(text = movieName, fontWeight = FontWeight.Bold, modifier = Modifier.padding(15.dp))
-            Text(text = movieVote, fontWeight = FontWeight.Bold, modifier = Modifier.padding(15.dp))
-        }
+            Row(modifier = Modifier.padding(15.dp)) {
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = "Star",
+                    tint = Color.Yellow,
+                    modifier = Modifier.size(25.dp)
+                )
+                Text(text = movieVote,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 4.dp))
+            }        }
         Text(text = movieOverview, modifier = Modifier
             .padding(vertical = 12.dp)
             .padding(15.dp))
