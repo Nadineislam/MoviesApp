62887ea67fc68159e6896b5296c5e859c3e6deef
Nadine Islam <nadeen.kamaal@gmail.com>
2024-01-09 17:19:29 +0200

    Moved TvCategoryScreen function to TvCategory activity

--- app/src/main/java/com/example/movieapp/movie_home_feature/presentation/components/TvCategoryItem.kt
+++ app/src/main/java/com/example/movieapp/movie_home_feature/presentation/components/TvCategoryItem.kt
@@ -1,27 +1,19 @@
 package com.example.movieapp.movie_home_feature.presentation.components
 
 import android.content.Intent
-import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -31,53 +23,10 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.rememberAsyncImagePainter
 import com.example.movieapp.core.utils.Constants
-import com.example.movieapp.core.utils.Resource
 import com.example.movieapp.movie_home_feature.data.remote.dto.Tv
 import com.example.movieapp.movie_home_feature.presentation.activities.MovieDetails
-import com.example.movieapp.movie_home_feature.presentation.viewmodel.TvViewModel
-
-@Composable
-fun TvCategoryScreen(viewModel: TvViewModel) {
-    val searchResult = viewModel.tvCategory.collectAsStateWithLifecycle()
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        when (val resource = searchResult.value) {
-            is Resource.Error -> {
-                val message = resource.message ?: "Error fetching movie"
-                Toast.makeText(LocalContext.current, message, Toast.LENGTH_LONG)
-                    .show()
-            }
-
-            is Resource.Loading -> {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator()
-                }
-
-            }
-
-            is Resource.Success -> {
-                val movieList = resource.data?.trendingTv
-                movieList?.let { tv ->
-                    LazyVerticalGrid(columns = GridCells.Adaptive(150.dp)) {
-                        items(tv) { tv ->
-                            TvItem(tv = tv)
-
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun TvItem(tv: Tv) {
